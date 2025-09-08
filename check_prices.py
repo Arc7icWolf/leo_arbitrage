e55673867f56db10e4d855e4b9632bedea5721af
Arc7icWolf <162563901+Arc7icWolf@users.noreply.github.com>
2025-09-08 11:17:42 +0000

    up

--- check_prices.py
+++ check_prices.py
@@ -7,7 +7,7 @@ def get_response(method, url, json=None):
     try:
         if method == "GET":
             response = requests.get(url)
-        else: # POST
+        else:  # POST
             response = requests.post(url, json=json)
         response.raise_for_status()
         return response.json()
@@ -42,17 +42,17 @@ def get_he_price():
 
 def get_maya_price(quantity):
     amount = Decimal(quantity)
-    token_amount = int(amount * (10 ** 8))
+    token_amount = int(amount * (10**8))
     url = (
-      f"https://mayanode.mayachain.info/mayachain/quote/swap?"
-      f"from_asset=ARB.LEO-0X93864D81175095DD93360FFA2A529B8642F76A6E"
-      f"&to_asset=ARB.ETH"
-      f"&amount={token_amount}"
-      f"&destination=0x1EdF9F4d2e98A2eb5DFeeC7f07c2e8b6C3FFaA4E"
-      f"&streaming_interval=3"
-      f"&streaming_quantity=0"
-      f"&liquidity_tolerance_bps=100"
-      f"&affiliate_bps=45&affiliate=wr"
+        f"https://mayanode.mayachain.info/mayachain/quote/swap?"
+        f"from_asset=ARB.LEO-0X93864D81175095DD93360FFA2A529B8642F76A6E"
+        f"&to_asset=ARB.ETH"
+        f"&amount={token_amount}"
+        f"&destination=0x1EdF9F4d2e98A2eb5DFeeC7f07c2e8b6C3FFaA4E"
+        f"&streaming_interval=3"
+        f"&streaming_quantity=0"
+        f"&liquidity_tolerance_bps=100"
+        f"&affiliate_bps=45&affiliate=wr"
     )
     maya_price = get_response("GET", url)
     return maya_price
@@ -61,8 +61,7 @@ def get_maya_price(quantity):
 def get_price(token):
     url = f"https://api.coingecko.com/api/v3/simple/price?ids={token}&vs_currencies=usd"
     price = get_response("GET", url)
-    return price['hive']['usd']
-
+    return price["hive"]["usd"]
 
 
 def compare_prices():
@@ -72,19 +71,17 @@ def compare_prices():
     token_amount = 100 / price
 
     he_price = get_he_price()
-    leo_price = he_price[0]['result'][0]['basePrice']
-    eth_price = he_price[1]['result'][0]['quotePrice']
+    leo_price = he_price[0]["result"][0]["basePrice"]
+    eth_price = he_price[1]["result"][0]["quotePrice"]
 
     leo = token_amount * float(leo_price) * 0.99 * 0.895
 
     maya_price = get_maya_price(leo)
 
-    eth_quantity = int(maya_price['expected_amount_out']) / (10 ** 8)
+    eth_quantity = int(maya_price["expected_amount_out"]) / (10**8)
 
     new_token_amount = eth_quantity * 0.99 * 0.999 * float(eth_price)
     print(new_token_amount)
-
-
 
 
 if __name__ == "__main__":
