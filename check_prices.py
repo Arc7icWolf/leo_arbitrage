0107176d9540dafbf28c124359bb484ada2cb8db
Arc7icWolf <162563901+Arc7icWolf@users.noreply.github.com>
2025-09-12 17:44:58 +0200

    Update check_prices.py

--- check_prices.py
+++ check_prices.py
@@ -111,7 +111,7 @@ def compare_prices():
     
     print(arb_leo_amount)
 
-    threshold = 1.12
+    threshold = 1.13
 
     if he_leo_amount > arb_leo_amount * threshold:
         print("HIVE --> LEO --> ARB.LEO --> ETH")
